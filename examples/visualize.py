74f7135df733d7b3bba31710624ee6997c553f51
fingoldin <vassilioskaxiras@gmail.com>
2022-04-07 20:44:36 -0400

    add comments

--- examples/visualize.py
+++ examples/visualize.py
@@ -3,10 +3,13 @@ from wvgsolver.parse import DielectricExtrusionFaceGDSParser, ObjectParser3D
 
 cavity = Cavity1D(load_path="cavity.obj")
 
-cavity.get_results("guidedness")[0]["sess_res"].show()
+# Display the Lumerical file associated with a simulation
+# cavity.get_results("guidedness")[0]["sess_res"].show()
 
 parsed = ObjectParser3D(cavity)
+# Display a 3D visualization of the cavity
 parsed.show()
 
 parsed = DielectricExtrusionFaceGDSParser(cavity, invert=True)
+# Display a 2D slice of the cavity, similarly to what we might output to a GDS file
 parsed.show()
