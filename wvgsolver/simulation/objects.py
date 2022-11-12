508a1bd253ce07fae58824d489b4ca649164ca8b
fingoldin <vassilioskaxiras@gmail.com>
2022-11-12 12:29:15 -0500

    add no vec

--- wvgsolver/simulation/objects.py
+++ wvgsolver/simulation/objects.py
@@ -575,7 +575,7 @@ class Cavity1D(Waveguide):
       "qzmin": qzmin,
       "qzmax": qzmax,
       "vmode": vmode,
-      "eremain": e / eref,
+      "eremain": e,
       "xyprofile": EField(xyprofile, ("x", "y")),
       "yzprofile": EField(yzprofile, ("y", "z"))
     }
