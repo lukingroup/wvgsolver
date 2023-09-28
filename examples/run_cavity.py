ecfa7776772deee82a6d77958f157582a015ef21
huanyanqi <36173574+huanyanqi@users.noreply.github.com>
2023-09-28 16:04:28 -0400

    make the file save to a directory different from the script

--- examples/run_cavity.py
+++ examples/run_cavity.py
@@ -17,20 +17,17 @@ from wvgsolver.engine import LumericalEngine
 
 #  Initialize Lumerical File Locations
 FDTDLoc = "D:\\Program Files\\Lumerical\\v232"  # '/n/sw/lumerical-2021-R2-2717-7bf43e7149_seas'
-save_dir = "C:\\Users\\Qi\\Desktop\\WvgSolverOutput"
-fsps_dir = os.path.join(save_dir, 'fsps')
+save_base_dir = "C:\\Users\\Qi\\Desktop\\WvgSolverOutput"
+
 
 nmirrsL = 7
 nmirrsR = 5
 ndefs = 5
-target_qx = 6000
 
-# Target resonance frequency (Hz)
-target_frequency = 406.7e12
-# Frequency of excitation light in simulation (Hz)
-source_frequency = 406.7e12
-# Threshold for how far the frequency can be off before we rerun
-rerun_thresh = 0.955
+target_frequency = 406.7e12 # Target resonance frequency (Hz)
+source_frequency = 406.7e12 # Frequency of excitation light in simulation (Hz)
+rerun_thresh = 0.955 # Threshold for how far the frequency can be off before we rerun
+target_qx = 6000 # Only used for computing fitness
 
 n_beam = 2.4028
 iter_count = 0
@@ -40,7 +37,7 @@ def gauss(x, x0, sigma):
     return np.exp(-((x - x0) / sigma) ** 2)
 
 
-def plot_geom(cavity, file_name, hide):
+def plot_geom(cavity, full_file_name, hide):
     plt.close()
     unit_cells = cavity.get_unit_cells()
 
@@ -50,19 +47,19 @@ def plot_geom(cavity, file_name, hide):
     for i,unit_cell in enumerate(unit_cells):
         size = unit_cell.get_size()
         structures = unit_cell.get_structures()
-        hx[i] = structures[1].radius*2
-        hy[i] = structures[1].radius2*2
+        hx[i] = 2 * structures[1].radius
+        hy[i] = 2 * structures[1].radius2
         a_s[i] = size.x
 
     x_coords = np.copy(a_s)
     x_coords = x_coords[:-1]
-    x_coords = np.insert(x_coords,0,0)
+    x_coords = np.insert(x_coords, 0, 0)
     x_coord = 0
     for i,a in enumerate(x_coords):
         x_coord += a
         x_coords[i] = x_coord
 
-    a_coords = np.array([(x+x_coords[i+1])/2 for i, x in enumerate(x_coords[:-1])])
+    a_coords = np.array([(x + x_coords[i+1]) / 2 for i, x in enumerate(x_coords[:-1])])
 
     plt.close()
 
@@ -72,14 +69,14 @@ def plot_geom(cavity, file_name, hide):
     plt.plot(x_coords, hx/a_s[5], 'go-', label='hx')
     plt.plot(x_coords, hy/a_s[5], 'bo-', label='hy')
 
-    plt.ylabel(r"$\mathregular{a}_{\mathregular{nominal}}$",fontsize=14)
-    plt.xlabel(r"Unit Cell Position ($\mu$m)",fontsize=12)
+    plt.ylabel(r"$\mathregular{a}_{\mathregular{nominal}}$", fontsize=14)
+    plt.xlabel(r"Unit Cell Position ($\mu$m)", fontsize=12)
     plt.legend(loc=8)
 
     fig = plt.gcf()
     fig.set_size_inches(6, 3)
     plt.tight_layout()
-    plt.savefig(file_name)
+    plt.savefig(full_file_name)
     if not hide:
         plt.show()
     plt.close()
@@ -89,8 +86,23 @@ def build_cavity(cavity_params):
     iter_count += 1
 
     maxDef, beam_w, hxL, hyL, hxR, hyR, aL, aR = cavity_params
-    print(cavity_params)
-    
+
+    cavity_name = "_".join([str(n) for n in cavity_params])
+    print(f"Cavity params: {cavity_params}")
+    print(f"Cavity name: {cavity_name}")
+
+    file_name = f"{log_name[:-4]}_{cavity_name}_{iter_count}"
+    subdir = file_name
+    subdir_path = os.path.join(save_base_dir, subdir)
+
+    if not os.path.exists(subdir_path):
+        os.mkdir(subdir_path)
+    full_file_name = os.path.join(subdir_path, file_name)
+
+    print(f"File name: {file_name}")
+    print(f"Save path: {subdir_path}")
+    print(f"Full file path: {full_file_name}")
+
     pcc_params = {
         'layer'               : 2,
         'aL'                  : aL,
@@ -111,7 +123,7 @@ def build_cavity(cavity_params):
     }
 
 
-    apex_half_angle = 50*np.pi/180
+    apex_half_angle = 50 * np.pi / 180
 
     cavity = sivp.AirholePCC_PeriodOnly(pcc_params)
     all_hx, all_hy, all_a = cavity.compute_geometry()
@@ -125,6 +137,7 @@ def build_cavity(cavity_params):
     beam_h = (beam_w / 2) * np.tan(np.pi/2 - apex_half_angle)
 
     # Specify working_path to save solved fsp file
+    fsps_dir = os.path.join(subdir_path, 'fsps')
     engine = LumericalEngine(mesh_accuracy=5, hide=hide, working_path=fsps_dir, lumerical_path=FDTDLoc,save_fsp=True)
 
     cavity_cells = []
@@ -137,7 +150,7 @@ def build_cavity(cavity_params):
                                 rot_angles=(np.pi/2, np.pi/2, 0))
 
         # offset the hole to respect the way we define the relevant lattice constant
-        cell_hole = CylinderStructure(Vec3(-a/2,0,0), beam_h, hx/2, DielectricMaterial(1, order=1, color="red"), radius2=hy/2)
+        cell_hole = CylinderStructure(Vec3(-a/2, 0, 0), beam_h, hx/2, DielectricMaterial(1, order=1, color="red"), radius2=hy/2)
         unit_cell = UnitCell(structures = [cell_box, cell_hole], size=cell_size, engine=engine)
         cavity_cells += [unit_cell]
 
@@ -154,21 +167,15 @@ def build_cavity(cavity_params):
       unit_cells=cavity_cells,
       structures=[TriStructure(Vec3(0), Vec3(beam_w, apex_half_angle, beam_length), 
                   DielectricMaterial(n_beam, order=2, color="gray"), rot_angles=(np.pi/2, np.pi/2, 0))], engine=engine, center_shift = shift)
-
-    cavity_name = "_".join([str(n) for n in cavity_params])
-    print(f"Cavity params: {cavity_params}")
-    print(f"Cavity name: {cavity_name}")
-
-    # By setting the save path here, the cavity will save itself after each simulation to this file
-    file_name = os.path.join(save_dir, f"{log_name[:-4]}_{cavity_name}_{iter_count}")
-    cavity.save(file_name + ".obj")
-    plot_geom(cavity, file_name + "_geom.png", hide)
-    return cavity, file_name
+    
+    cavity.save(full_file_name + ".obj")
+    plot_geom(cavity, full_file_name + "_geom.png", hide)
+    return cavity, full_file_name
 
 def fitness(cavity_params):
     global source_frequency
     
-    cavity, file_name = build_cavity(cavity_params)
+    cavity, full_file_name = build_cavity(cavity_params)
     
     man_mesh = MeshRegion(BBox(Vec3(0), Vec3(10e-6, 0.7e-6, 0.4e-6)), 12e-9, dy=None, dz=None)
 
@@ -199,8 +206,6 @@ def fitness(cavity_params):
     print(f"Purcell factor: {purcell:.0f}.")
 
     wavelen_pen = gauss(F, target_frequency, 4e12)
-
-    # TODO: check if / 4 is correct
     qx_pen = (gauss(qx, target_qx, 120000) + gauss(qx, target_qx, 60000) +
               gauss(qx, target_qx, 30000) + gauss(qx, target_qx, 2000) / 4)
 
@@ -217,10 +222,10 @@ def fitness(cavity_params):
                                                        qscat, qtot, vmode, vmode_copy, F]))
         np.savetxt(f, step_info.reshape(1, step_info.shape[0]), fmt='%.6f')
 
-    r1["xyprofile"].save(file_name + "_xy.png", 
+    r1["xyprofile"].save(full_file_name + "_xy.png", 
                          title=f"Q = {qtot:.0f} \nQ_scat = {qscat:.04} Qx = {qx:.0f}\nV = {vmode_copy:.3f}")
-    r1["yzprofile"].save(file_name + "_yz.png",
-                         title=f"Q = {qtot:.0f} Q_scat = {qscat:.04}\n Qx1 = {qx1:.0f} Qx2 = {qx2:.0f}\nV = {vmode_copy:.3f} "+r"$\lambda$"+f" = {wavelen:.1f}")
+    r1["yzprofile"].save(full_file_name + "_yz.png",
+                         title=f"Q = {qtot:.0f} Q_scat = {qscat:.04}\n Qx1 = {qx1:.0f} Qx2 = {qx2:.0f}\nV = {vmode_copy:.3f} "+r"$\lambda$"+f" = {wavelen:.3f}")
 
     # Plot the quasipotential
     # r2 = cavity.simulate("quasipotential", target_freq=target_frequency, freqs=(0.25e15, 0.7e15, 100000), window_pos = 0)
@@ -245,22 +250,15 @@ if len(sys.argv) == 2:
 else:
     design_name = input("Design name?")
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-log_name = os.path.join(save_dir, f"{timestamp}-{design_name}-cavity_run-{nmirrsL}-{ndefs}-{nmirrsR}.txt")
+log_name = f"{timestamp}-{design_name}-cavity_run-{nmirrsL}-{ndefs}-{nmirrsR}.txt"
 
 # maxDef, beam_w, hxL, hyL, hxR, hyR, aL, aR
 p0 = np.array([0.14276, 0.483143,
                0.108918, 0.164633,
                0.108918, 0.164633,
-               0.273535, 0.251509])
+               0.273535, 0.251509]) * 0.99662
 
-# p0 = np.array([0.1392,    0.482,
-#                0.1135849, 0.1605274,
-#                0.1135849, 0.1605274,
-#                0.2717,    0.2502])
-
-
-
-with open(log_name, "ab") as f:
+with open(os.path.join(save_base_dir, log_name), "ab") as f:
     f.write(b"maxDef    beam_w    hxL    hyL    hxR    hyR    aL    aR    fitness    wavelen_pen    purcell    qxmin    qxmax    qscat    qtot    vmode    vmode_copy    freq")
 
 fitness_result = fitness(p0)
